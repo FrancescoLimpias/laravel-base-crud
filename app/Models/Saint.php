60a07d0cdd49badaaa06424b7c26c122f008d16a
Francesco Limpias <francescolimpias@gmail.com>
2023-02-06 07:49:11 +0100

    Fixed Saint model

--- app/Models/Saint.php
+++ app/Models/Saint.php
@@ -8,9 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Saint extends Model
 {
     use HasFactory;
-
-    private $name;
-    private $birthPlace;
-    private $sanctificationDate;
-    private $miraclesNumber;
 }
